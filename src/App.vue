66683b12bbd4035c29a6e7725aa14284599d12d3
Dalmontron05 <vexr2dts85338@gmail.com>
2026-04-06 18:59:00 -0700

    almost done with hoem page content

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,6 @@
 <template>
     <main>
     <h1>Siege-O-Namics</h1>
-    <!-- This won't have a minimum padding for some reason -->
     <i style="padding-bottom: 3rem;">It's Like Wikipedia, but for Rainbow Six Siege Operators.</i>
     <p>Ubislop's official website is... well, it's slop. Tom Clancy's Rainbow Six: Siege is a bad game with a steep learning curve. As someone who's played for years with 2500 hours in the game, I can't in good faith reccomend it to someone.</p>
     <img src="/public/UbiTrash.gif" alt="Gif of Ubisoft logo flying into a trashcan">
@@ -12,6 +11,9 @@
     <p>Did you know anyone in Tubarão's gadget doesn't glow yellow in glaz's thermal scope? Have you seen that Thermite can breach a wall that's electrified by breaching a perpendicular wall or the floor above? Were you aware of how shockingly easy it is to EMP a C4 with Thatcher's gadget?</p>
     <p>Mastering these situational, yet important scenarios are what seperate the great from the good. Knowing the full potential of what operators you main can do is a low investment, high reward method of drastically improving your skill and fun within siege.</p>
     </main>
+
+    <!-- make this shit resize -->
+    <img src="/public/Operator-Tierlist.png" alt="Operator tierlist ranking"></img>"
 </template>
 
 <style>
@@ -22,10 +24,11 @@ main {
 }
 
 main img {
-    max-width: 80%;
+    display: block;
+    width: min(60%, 520px);
+    max-width: 100%;
     height: auto;
-    size: 100px;
-    justify-content: center;
+    margin: 0 auto;
 }
 </style>
 
