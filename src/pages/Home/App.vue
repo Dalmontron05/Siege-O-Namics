8b30bd4539c692921ea1832fbc8fd1aa0f2e29a9
Dalmontron05 <vexr2dts85338@gmail.com>
2026-04-13 18:14:20 -0700

    creating multiple lages, moved assets

--- src/pages/Home/App.vue
+++ src/pages/Home/App.vue
@@ -6,12 +6,11 @@
     <h1>Siege-O-Namics</h1>
     <i style="padding-bottom: 3rem;">It's Like Wikipedia, but for Rainbow Six Siege Operators.</i>
     <p>Ubislop's official website is... well, it's slop. Tom Clancy's Rainbow Six: Siege is a bad game with a steep learning curve. As someone who's played for years with 2500 hours in the game, I can't in good faith reccomend it to someone.</p>
-    <img src="/public/UbiTrash.gif" alt="Gif of Ubisoft logo flying into a trashcan">
+    <img src="/src/assets/Images/UbiTrash.gif" alt="Gif of Ubisoft logo flying into a trashcan">
     <p>That being said, R6 is a one of a kind. I (for the most part) love the game and continue to play it consistantly to this day. This project will help if you're a beginner, but it's catered more towards intermediate players that want to take the next step in their competitive gameplay.</p>
     <p>Did you know anyone in Tubarão's gadget doesn't glow yellow in glaz's thermal scope? Have you seen that Thermite can breach a wall that's electrified by breaching a perpendicular wall or the floor above? Were you aware of how shockingly easy it is to EMP a C4 with Thatcher's gadget?</p>
     <p>Mastering these situational, yet important scenarios are what seperate the great from the good. Knowing the full potential of what operators you main can do is a low investment, high reward method of drastically improving your skill and fun within siege.</p>
-    <p style="padding-bottom: 3rem;">To show you I know what I'm talking about, here's the objectively only real tierlist.</p>
-    <img src="/public/Operator-Tierlist.png" alt="Operator tierlist ranking">
+    <img src="/src/assets/Images/Operator-Tierlist.png" alt="Operator tierlist ranking">
     </main>
 </template>
 
